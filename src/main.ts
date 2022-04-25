508c02c102683deab1adc0c09d45c043e0e1a919
Abraham Schilling <AbrahamSchilling@gmail.com>
2022-04-25 22:57:44 +0200

    Register for events before creating the stream

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,19 @@
 import fs from "fs"
 import { doSomethingWithProducts } from "./doSomethingWithProducts"
+import { events } from "./events"
 import { mapXmlProducts } from "./mapXmlProducts"
 import { parser } from "./xmlStreamingParser"
 
 async function main() {
-  fs.createReadStream("products.xml")
-    .pipe(parser)
-    .on("finish", () => console.log("done"))
-
   mapXmlProducts()
   doSomethingWithProducts()
+
+  fs.createReadStream("products.xml")
+    .pipe(parser)
+    .on("finish", () => {
+      events.emit("done")
+      console.log("done")
+    })
 }
 
 main().catch((error) => {
